c059a6d8f52b2087f810b0d12a31e472a649d61d
John Thornton <dev@gnipsel.com>
2023-04-01 07:48:07 -0500

    add reload and verify firmware
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- mesact/src/libmesact/flash.py
+++ mesact/src/libmesact/flash.py
@@ -1,7 +1,7 @@
-import subprocess
+import os, subprocess
 from subprocess import Popen, PIPE
 
-from PyQt5.QtWidgets import QInputDialog, QLineEdit, QDialogButtonBox
+from PyQt5.QtWidgets import QInputDialog, QLineEdit, QDialogButtonBox, qApp
 
 def check_emc():
 	if "0x48414c32" in subprocess.getoutput('ipcs'):
@@ -107,7 +107,7 @@ def readpd(parent):
 		if check_ip(parent):
 			ipAddress = parent.ipAddressCB.currentText()
 			p = Popen(['mesaflash', '--device', board, '--addr', ipAddress, '--print-pd'],
-				stdin=PIPE, stderr=PIPE, stdout=PIPE, universal_newlines=True)
+				stdin=PIPE, stderr=PIPE, stdout=PIPE, text=True)
 			prompt = p.communicate()
 		else:
 			return
@@ -118,11 +118,107 @@ def readpd(parent):
 			parent.password = password
 		if parent.password != None:
 			p = Popen(['sudo', '-S', 'mesaflash', '--device', parent.board, '--print-pd'],
-				stdin=PIPE, stderr=PIPE, stdout=PIPE, universal_newlines=True)
+				stdin=PIPE, stderr=PIPE, stdout=PIPE, text=True)
 			prompt = p.communicate(parent.password + '\n')
 	if prompt:
 		getResults(parent, prompt, p.returncode, 'firmwarePTE', 'Read Pin Descriptions')
 
+def flashCard(parent):
+	prompt = None
+	board = parent.boardCB.currentData()
+	arguments = []
+	if check_emc():
+		parent.errorMsgOk(f'LinuxCNC must NOT be running\n to flash the {parent.board}', 'Error')
+		return
+	if parent.firmwareCB.currentData():
+		firmware = os.path.basename(parent.firmwareCB.currentData())
+		parent.firmwarePTE.clear()
+		parent.firmwarePTE.setPlainText(f'Flashing: {firmware} to {parent.board}')
+		qApp.processEvents()
+		firmware = os.path.join(parent.lib_path, parent.firmwareCB.currentData())
+		if parent.boardType == 'eth':
+			if check_ip(parent):
+				ipAddress = parent.ipAddressCB.currentText()
+				p = Popen(['mesaflash', '--device', board, '--addr', ipAddress,
+					'--write', firmware], stdin=PIPE, stderr=PIPE, stdout=PIPE, text=True)
+				prompt = p.communicate()
+			else:
+				return
 
+		elif parent.boardType == 'pci':
+			if not parent.password:
+				password = getPassword(parent)
+				parent.password = password
+			if parent.password != None:
+				p = Popen(['sudo', '-S', 'mesaflash', '--device', parent.board, '--write', firmware],
+					stdin=PIPE, stderr=PIPE, stdout=PIPE, text=True)
+				prompt = p.communicate(parent.password + '\n')
+
+		if prompt:
+			getResults(parent, prompt, p.returncode, 'firmwarePTE', 'Flash')
+
+	else:
+		parent.errorMsgOk('A firmware must be selected', 'Error!')
+		return
+
+def reloadCard(parent):
+	prompt = None
+	board = parent.boardCB.currentData()
+	if check_emc():
+		parent.errorMsgOk(f'LinuxCNC must NOT be running\n to reload the {board}', 'Error')
+		return
+	if parent.boardType == 'eth':
+		if check_ip(parent):
+			ipAddress = parent.ipAddressCB.currentText()
+			p = Popen(['mesaflash', '--device', board, '--addr', ipAddress, '--reload'],
+				stdin=PIPE, stderr=PIPE, stdout=PIPE, text=True)
+			prompt = p.communicate()
+		else:
+			return
+
+	elif parent.boardType == 'pci':
+		if not parent.password:
+			password = getPassword(parent)
+			parent.password = password
+		if parent.password != None:
+			p = Popen(['sudo', '-S', 'mesaflash', '--device', parent.board, '--reload'],
+				stdin=PIPE, stderr=PIPE, stdout=PIPE, text=True)
+			prompt = p.communicate(parent.password + '\n')
+
+	if prompt:
+		getResults(parent, prompt, p.returncode, 'firmwarePTE', 'Reload Firmware')
+		parent.firmwarePTE.appendPlainText('Wait 30 seconds before Verifying the Firmware')
+
+def verifyFirmware(parent):
+	prompt = None
+	board = parent.boardCB.currentData()
+	if check_emc():
+		parent.errorMsgOk(f'LinuxCNC must NOT be running\n to verify the {board}', 'Error')
+		return
+	if parent.firmwareCB.currentData():
+		firmware = os.path.join(parent.lib_path, parent.firmwareCB.currentData())
+		if parent.boardType == 'eth':
+			if check_ip(parent):
+				ipAddress = parent.ipAddressCB.currentText()
+				p = Popen(['mesaflash', '--device', board, '--addr', ipAddress, '--verify', firmware],
+					stdin=PIPE, stderr=PIPE, stdout=PIPE, text=True)
+				prompt = p.communicate()
+			else:
+				return
+
+		elif parent.boardType == 'pci':
+			if not parent.password:
+				password = getPassword(parent)
+				parent.password = password
+			if parent.password != None:
+				p = Popen(['sudo', '-S', 'mesaflash', '--device', board, '--verify', firmware],
+					stdin=PIPE, stderr=PIPE, stdout=PIPE, text=True)
+				prompt = p.communicate(parent.password + '\n')
+
+		if prompt:
+			getResults(parent, prompt, p.returncode, 'firmwarePTE', 'Verify Firmware')
+	else:
+		parent.errorMsgOk('A firmware must be selected', 'Error!')
+		return
 
 
