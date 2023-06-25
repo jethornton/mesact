aafa927bb71da0023fa1021848339997280ef342
John Thornton <dev@gnipsel.com>
2023-06-25 07:44:17 -0500

    add more tests
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- mesact/src/libmesact/testing.py
+++ mesact/src/libmesact/testing.py
@@ -15,6 +15,8 @@ def default_settings(parent):
 	parent.set_7i96s_pb.setEnabled(True)
 	parent.set_7i96s_7i76_pb.setEnabled(True)
 	parent.set_7i96s_7i77_pb.setEnabled(True)
+	parent.set_7i92t_7i76_pb.setEnabled(True)
+	parent.set_7i92t_7i77_pb.setEnabled(True)
 	parent.x_config_pb.setEnabled(True)
 	parent.xyz_config_pb.setEnabled(True)
 	parent.xyyz_config_pb.setEnabled(True)
@@ -32,6 +34,16 @@ def set_7i96s_7i77(parent):
 	parent.boardCB.setCurrentIndex(parent.boardCB.findData('7i96s'))
 	parent.ipAddressCB.setCurrentIndex(parent.ipAddressCB.findData('10.10.10.10'))
 	parent.daughterCB_0.setCurrentIndex(parent.daughterCB_0.findData('7i77'))
+
+def set_7i92t_7i76(parent):
+	parent.boardCB.setCurrentIndex(parent.boardCB.findData('7i92t'))
+	parent.ipAddressCB.setCurrentIndex(parent.ipAddressCB.findData('10.10.10.10'))
+	parent.daughterCB_1.setCurrentIndex(parent.daughterCB_1.findData('7i76'))
+
+def set_7i92t_7i77(parent):
+	parent.boardCB.setCurrentIndex(parent.boardCB.findData('7i92t'))
+	parent.ipAddressCB.setCurrentIndex(parent.ipAddressCB.findData('10.10.10.10'))
+	parent.daughterCB_1.setCurrentIndex(parent.daughterCB_1.findData('7i77'))
 
 def x_config(parent):
 	axes = ['X']
