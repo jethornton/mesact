793755d8aa17c282e13abf8a4d60967191ccd85c
John Thornton <dev@gnipsel.com>
2023-06-27 07:51:18 -0500

    show correct output tabs for daughter tabs
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- mesact/src/libmesact/daughters.py
+++ mesact/src/libmesact/daughters.py
@@ -1,42 +1,29 @@
 
-'''
-7i76 5 step/dir 32 inputs 16 outputs 1 spindle 1 encoder
-7i77 6 analog  32 inputs 16 outputs 1 spindle 1 encoder
-7i78 4 step/dir 1 spindle 1 encoder
-parent.mainTW.setTabText(4, 'P1')
-parent.mainTW.setTabText(5, 'P2')
-
-'''
-
 def changed(parent):
+	index = int(parent.sender().objectName()[-1])
 	if parent.sender().currentData(): # daughter card selected
-		index = int(parent.sender().objectName()[-1])
+		board = parent.sender().currentData()
 		tab = int(parent.sender().objectName()[-1]) + 4
-		print(f'Tab: {tab}')
-		if parent.sender().currentData():
-			#print(parent.sender().objectName())
-			stepper = ['7i76', '7i78']
-			analog = ['7i77']
-			board = parent.sender().currentData()
-			parent.mainTW.setTabVisible(tab, True)
-			connector = getattr(parent, f'daughterLB_{index}').text()
-			#print(connector)
-			parent.mainTW.setTabText(tab, f'{board}')
-			# daughter_info_pte_0
-			info = (f'Connector: {connector}'
-			)
-			getattr(parent, f'daughter_info_pte_{index}').setPlainText(info)
-			# getattr(parent, f'c0_settings_{i}').setTabVisible(3, False)
-			# getattr(parent, f'c0_settings_{i}').setTabVisible(4, False)
+		card = int(parent.sender().objectName()[-1]) + 1
+		stepper = ['7i76', '7i78', '7i85s']
+		analog = ['7i77', '7i85']
+		#print(f'tab: {tab}')
+		#print(f'index: {index}')
+		#print(f'card: {card}')
+		#print(parent.sender().objectName())
+		parent.mainTW.setTabVisible(tab, True)
+		connector = getattr(parent, f'daughterLB_{index}').text()
+		#print(f'connector: {connector}')
+		parent.mainTW.setTabText(tab, f'{board}')
+		# daughter_info_pte_0
+		info = (f'Connector: {connector}'
+		)
+		getattr(parent, f'daughter_info_pte_{index}').setPlainText(info)
+		for i in range(6): # hide stepper tab
+			if board in analog: getattr(parent, f'c{card}_settings_{i}').setTabVisible(2, False)
+			if board in stepper: getattr(parent, f'c{card}_settings_{i}').setTabVisible(3, False)
+			if board in stepper: getattr(parent, f'c{card}_settings_{i}').setTabVisible(4, False)
 
-			for i in range(6):
-				if board in stepper:
-					getattr(parent, f'c{index + 1}_stepgenGB_{i}').setVisible(True)
-					getattr(parent, f'c{index + 1}_analogGB_{i}').setVisible(False)
-				elif board in analog:
-					getattr(parent, f'c{index + 1}_stepgenGB_{i}').setVisible(False)
-					getattr(parent, f'c{index + 1}_analogGB_{i}').setVisible(True)
-		else:
-			parent.mainTW.setTabVisible(4 + index, False)
-
+	else:
+		parent.mainTW.setTabVisible(4 + index, False)
 
