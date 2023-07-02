08dbd41d61e448616c6fd7a7ca13c327b137ee86
John Thornton <dev@gnipsel.com>
2023-07-02 17:48:18 -0500

    remove unused code
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- mesact/src/libmesact/daughters.py
+++ mesact/src/libmesact/daughters.py
@@ -47,38 +47,6 @@ def changed(parent):
 			else:
 				getattr(parent, f'c{daughter}_settings_{i}').setTabVisible(4, False)
 
-
-		#c1_settings_0 stepgen 2 analog 3 encoder 4
-
-		#parent.c1_JointTW.setTabVisible(6, False)
-
-		#print(type(axis))
-		#print(axis)
-
-
-		'''
-
-				print(f'c{index}_JointTW')
-
-		#print(f'board: {board}')
-		#print(f'tab: {tab}')
-		#print(f'index: {index}')
-		#print(f'connector: {connector}')
-		#print(parent.sender().objectName())
-
-		stepper = ['7i76', '7i78', '7i85s']
-		analog = ['7i77', '7i85']
-		connector = getattr(parent, f'daughterLB_{index}').text()
-		# daughter_info_pte_0
-		info = (f'Connector: {connector}'
-		)
-		getattr(parent, f'daughter_info_pte_{index}').setPlainText(info)
-		for i in range(6): # hide stepper tab
-			if board in analog: getattr(parent, f'c{card}_settings_{i}').setTabVisible(2, False)
-			if board in stepper: getattr(parent, f'c{card}_settings_{i}').setTabVisible(3, False)
-			if board in stepper: getattr(parent, f'c{card}_settings_{i}').setTabVisible(4, False)
-		'''
-
 	else:
 		parent.mainTW.setTabVisible(4 + index, False)
 
