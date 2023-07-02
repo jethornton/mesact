d16b1bfad17bd05305c074841d6631af0f560649
John Thornton <dev@gnipsel.com>
2023-07-02 17:43:45 -0500

    finish up daughter board dictionary
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- mesact/src/libmesact/daughters.py
+++ mesact/src/libmesact/daughters.py
@@ -8,14 +8,20 @@ def changed(parent):
 		board = parent.sender().currentData()
 		tab = int(parent.sender().objectName()[-1]) + 4
 		connector = int(parent.sender().objectName()[-1]) + 1
-		cards = {'7i76':{'axis':5, 'stepgen':5, 'analog':0, 'encoder':0},
-			'7i77':{'axis':6, 'stepgen':0, 'analog':6, 'encoder':6}}
+		cards = {'7i76':{'axis':5, 'stepgen':5, 'analog':0, 'encoder':0, 'spinenc':1, 'spinana':1},
+			'7i77':{'axis':6, 'stepgen':0, 'analog':6, 'encoder':6, 'spinenc':1, 'spinana':1},
+			'7i78':{'axis':4, 'stepgen':4, 'analog':0, 'encoder':0, 'spinenc':1, 'spinana':1},
+			'7i85':{'axis':4, 'stepgen':4, 'analog':0, 'encoder':0, 'spinenc':1, 'spinana':1},
+			'7i85s':{'axis':4, 'stepgen':4, 'analog':0, 'encoder':4, 'spinenc':1, 'spinana':1}
+			}
 		parent.mainTW.setTabVisible(tab, True)
-		parent.mainTW.setTabText(tab, f'{board}')
+		parent.mainTW.setTabText(tab, f'{parent.sender().currentText()}')
 		axis = cards[board]['axis']
 		stepgen = cards[board]['stepgen']
 		analog = cards[board]['analog']
 		encoder = cards[board]['encoder']
+		spinenc = cards[board]['spinenc']
+		spinana = cards[board]['spinana']
 
 		for i in range(1,7): # show/hide axis tabs
 			if i <= axis:
