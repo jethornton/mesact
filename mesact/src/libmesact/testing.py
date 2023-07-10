c7706b0dfac9cbc33c3567af81d501806826db48
John Thornton <dev@gnipsel.com>
2023-07-10 12:52:49 -0500

    remove some test configs
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- mesact/src/libmesact/testing.py
+++ mesact/src/libmesact/testing.py
@@ -17,18 +17,27 @@ def default_settings(parent):
 	parent.set_7i96s_7i77_pb.setEnabled(True)
 	parent.set_7i92t_7i76_pb.setEnabled(True)
 	parent.set_7i92t_7i77_pb.setEnabled(True)
-	parent.x_config_pb.setEnabled(True)
-	parent.xyz_config_pb.setEnabled(True)
-	parent.xyyz_config_pb.setEnabled(True)
 
 def set_7i96s(parent):
 	parent.boardCB.setCurrentIndex(parent.boardCB.findData('7i96s'))
 	parent.ipAddressCB.setCurrentIndex(parent.ipAddressCB.findData('10.10.10.10'))
+	set_joints(parent, 0, ['X', 'Y', 'Z'])
+	parent.c0_homeSequence_0.setText('2')
+	parent.c0_homeSequence_1.setText('1')
+	parent.c0_homeSequence_2.setText('0')
 
 def set_7i96s_7i76(parent):
 	parent.boardCB.setCurrentIndex(parent.boardCB.findData('7i96s'))
 	parent.ipAddressCB.setCurrentIndex(parent.ipAddressCB.findData('10.10.10.10'))
 	parent.daughterCB_0.setCurrentIndex(parent.daughterCB_0.findData('7i76'))
+	set_joints(parent, 0, ['X', 'Y', 'Z'])
+	set_joints(parent, 1, ['U', 'V', 'W'])
+	parent.c0_homeSequence_0.setText('2')
+	parent.c0_homeSequence_1.setText('1')
+	parent.c0_homeSequence_2.setText('0')
+	parent.c1_homeSequence_0.setText('5')
+	parent.c1_homeSequence_1.setText('4')
+	parent.c1_homeSequence_2.setText('3')
 
 def set_7i96s_7i77(parent):
 	parent.boardCB.setCurrentIndex(parent.boardCB.findData('7i96s'))
@@ -39,35 +48,21 @@ def set_7i92t_7i76(parent):
 	parent.boardCB.setCurrentIndex(parent.boardCB.findData('7i92t'))
 	parent.ipAddressCB.setCurrentIndex(parent.ipAddressCB.findData('10.10.10.10'))
 	parent.daughterCB_1.setCurrentIndex(parent.daughterCB_1.findData('7i76'))
+	set_joints(parent, 2, ['X', 'Y', 'Z'])
+	parent.c2_homeSequence_0.setText('2')
+	parent.c2_homeSequence_1.setText('1')
+	parent.c2_homeSequence_2.setText('0')
 
 def set_7i92t_7i77(parent):
 	parent.boardCB.setCurrentIndex(parent.boardCB.findData('7i92t'))
 	parent.ipAddressCB.setCurrentIndex(parent.ipAddressCB.findData('10.10.10.10'))
 	parent.daughterCB_1.setCurrentIndex(parent.daughterCB_1.findData('7i77'))
+	set_joints(parent, 2, ['X', 'Y', 'Z'])
+	parent.c2_homeSequence_0.setText('2')
+	parent.c2_homeSequence_1.setText('1')
+	parent.c2_homeSequence_2.setText('0')
 
-def x_config(parent):
-	axes = ['X']
-	set_joints(parent, axes)
-	sequence = len(axes) -1
-	parent.c0_homeSequence_0.setText('0')
-
-def xyz_config(parent):
-	axes = ['X', 'Y', 'Z']
-	set_joints(parent, axes)
-	sequence = len(axes) -1
-	parent.c0_homeSequence_0.setText('2')
-	parent.c0_homeSequence_1.setText('1')
-	parent.c0_homeSequence_2.setText('0')
-
-def xyyz_config(parent):
-	axes = ['X', 'Y', 'Y', 'Z']
-	set_joints(parent, axes)
-	parent.c0_homeSequence_0.setText('2')
-	parent.c0_homeSequence_1.setText('-1')
-	parent.c0_homeSequence_2.setText('-1')
-	parent.c0_homeSequence_3.setText('0')
-
-def set_joints(parent, axes):
+def set_joints(parent, card, axes):
 	# value = <value_if_true> if <expression> else <value_if_false>
 	mb = parent.boardCB.currentText()
 	p1 = parent.daughterCB_0.currentData()
@@ -78,18 +73,19 @@ def set_joints(parent, axes):
 	name = mb if mb else ''
 	parent.configNameLE.setText(f'{mb}{p1}{p2} {"".join(axes)}')
 	for joint, axis in enumerate(axes):
-		getattr(parent, f'c0_scale_{joint}').setText('1000')
-		getattr(parent, f'c0_axis_{joint}').setCurrentIndex(getattr(parent, f'c0_axis_{joint}').findData(axis))
+		#print(card, joint)
+		getattr(parent, f'c{card}_scale_{joint}').setText('1000')
+		getattr(parent, f'c{card}_axis_{joint}').setCurrentIndex(getattr(parent, f'c0_axis_{joint}').findData(axis))
 		if axis == 'Z':
-			getattr(parent, f'c0_min_limit_{joint}').setText('-5')
-			getattr(parent, f'c0_max_limit_{joint}').setText('0')
+			getattr(parent, f'c{card}_min_limit_{joint}').setText('-5')
+			getattr(parent, f'c{card}_max_limit_{joint}').setText('0')
 		else:
-			getattr(parent, f'c0_min_limit_{joint}').setText('0')
-			getattr(parent, f'c0_max_limit_{joint}').setText('10')
-		getattr(parent, f'c0_max_vel_{joint}').setText('1')
-		getattr(parent, f'c0_max_accel_{joint}').setText('4')
-		getattr(parent, f'c0_pidDefault_{joint}').click()
-		getattr(parent, f'c0_ferrorDefault_{joint}').click()
-		getattr(parent, f'c0_drive_{joint}').setCurrentIndex(getattr(parent, f'c0_drive_{joint}').findText('Gecko 203v'))
+			getattr(parent, f'c{card}_min_limit_{joint}').setText('0')
+			getattr(parent, f'c{card}_max_limit_{joint}').setText('10')
+		getattr(parent, f'c{card}_max_vel_{joint}').setText('1')
+		getattr(parent, f'c{card}_max_accel_{joint}').setText('4')
+		getattr(parent, f'c{card}_pidDefault_{joint}').click()
+		getattr(parent, f'c{card}_ferrorDefault_{joint}').click()
+		getattr(parent, f'c{card}_drive_{joint}').setCurrentIndex(getattr(parent, f'c{card}_drive_{joint}').findText('Gecko 203v'))
 
 
