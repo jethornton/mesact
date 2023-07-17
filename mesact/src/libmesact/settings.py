c79e58ce417a440970ba30e0fd770158d5472973
John Thornton <dev@gnipsel.com>
2023-07-17 13:23:02 -0500

    comment out debug print
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- mesact/src/libmesact/settings.py
+++ mesact/src/libmesact/settings.py
@@ -2,7 +2,7 @@
 # Do all config settings here.
 
 def update_settings(parent):
-	print('update_settings')
+	#print('update_settings')
 	parent.settings.setValue('no_nag_firmware', parent.no_check_firmware_cb.isChecked())
 	
 
