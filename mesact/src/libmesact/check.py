e2b4ca629386737ae2332f569c3c66502e4a951b
John Thornton <dev@gnipsel.com>
2023-08-17 07:37:22 -0500

    add checks for stepgen and analog
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- mesact/src/libmesact/check.py
+++ mesact/src/libmesact/check.py
@@ -114,6 +114,70 @@ def checkit(parent):
 				if getattr(parent, f'c{i}_max_accel_{j}').text() == '':
 					tabError = True
 					configErrors.append(f'\tJoint {j} Max Accel must not be blank')
+				if getattr(parent, f'c{i}_p_{j}').text() == '':
+					tabError = True
+					configErrors.append(f'\tJoint {j} PID P must not be blank')
+				if getattr(parent, f'c{i}_i_{j}').text() == '':
+					tabError = True
+					configErrors.append(f'\tJoint {j} PID I must not be blank')
+				if getattr(parent, f'c{i}_d_{j}').text() == '':
+					tabError = True
+					configErrors.append(f'\tJoint {j} PID D must not be blank')
+				if getattr(parent, f'c{i}_ff0_{j}').text() == '':
+					tabError = True
+					configErrors.append(f'\tJoint {j} PID FF0 must not be blank')
+				if getattr(parent, f'c{i}_ff1_{j}').text() == '':
+					tabError = True
+					configErrors.append(f'\tJoint {j} PIF FF1 must not be blank')
+				if getattr(parent, f'c{i}_ff2_{j}').text() == '':
+					tabError = True
+					configErrors.append(f'\tJoint {j} PID FF2 must not be blank')
+				if getattr(parent, f'c{i}_deadband_{j}').text() == '':
+					tabError = True
+					configErrors.append(f'\tJoint {j} PID Deadband must not be blank')
+				if getattr(parent, f'c{i}_bias_{j}').text() == '':
+					tabError = True
+					configErrors.append(f'\tJoint {j} PID Bias must not be blank')
+				if getattr(parent, f'c{i}_maxOutput_{j}').text() == '':
+					tabError = True
+					configErrors.append(f'\tJoint {j} PID Max Output must not be blank')
+				if getattr(parent, f'c{i}_maxError_{j}').text() == '':
+					tabError = True
+					configErrors.append(f'\tJoint {j} PID Max Error must not be blank')
+				if getattr(parent, f'c{i}_min_ferror_{j}').text() == '':
+					tabError = True
+					configErrors.append(f'\tJoint {j} Min Following Error must not be blank')
+				if getattr(parent, f'c{i}_max_ferror_{j}').text() == '':
+					tabError = True
+					configErrors.append(f'\tJoint {j} Max Following Error must not be blank')
+
+				if getattr(parent, f'c{i}_settings_{j}').isTabVisible(2): # Stepgen Tab
+					if getattr(parent, f'c{i}_drive_{j}').text() == '':
+						tabError = True
+						configErrors.append(f'\tJoint {j} Stepgen Type must not be blank')
+					if getattr(parent, f'c{i}_StepTime_{j}').text() == '':
+						tabError = True
+						configErrors.append(f'\tJoint {j} Stepgen Step Time must not be blank')
+					if getattr(parent, f'c{i}_StepSpace_{j}').text() == '':
+						tabError = True
+						configErrors.append(f'\tJoint {j} Stepgen Step Space must not be blank')
+					if getattr(parent, f'c{i}_DirSetup_{j}').text() == '':
+						tabError = True
+						configErrors.append(f'\tJoint {j} Stepgen Direction Setup must not be blank')
+					if getattr(parent, f'c{i}_DirHold_{j}').text() == '':
+						tabError = True
+						configErrors.append(f'\tJoint {j} Stepgen Direction Hold must not be blank')
+
+				if getattr(parent, f'c{i}_settings_{j}').isTabVisible(3): # Analog Tab
+					if getattr(parent, f'c{i}_analogMinLimit_{j}').text() == '':
+						tabError = True
+						configErrors.append(f'\tJoint {j} Analog Min Limit must not be blank')
+					if getattr(parent, f'c{i}_analogMaxLimit_{j}').text() == '':
+						tabError = True
+						configErrors.append(f'\tJoint {j} Analog Max Limit must not be blank')
+					if getattr(parent, f'c{i}_analogScaleMax_{j}').text() == '':
+						tabError = True
+						configErrors.append(f'\tJoint {j} Analog Scale Max must not be blank')
 
 	# check I/O for errors
 	# get the joints
