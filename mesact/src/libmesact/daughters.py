755a14fabfc1c9105ebbbadc916f5106fc5f5c27
John Thornton <dev@gnipsel.com>
2023-08-20 07:09:15 -0500

    work on daughter cards
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- mesact/src/libmesact/daughters.py
+++ mesact/src/libmesact/daughters.py
@@ -8,11 +8,12 @@ def changed(parent):
 		tab = int(parent.sender().objectName()[-1]) + 4
 		connector = int(parent.sender().objectName()[-1]) + 1
 		getattr(parent, f'c{connector}_JointTW').setTabText(0, parent.sender().currentText())
-		cards = {'7i76':{'axis':5, 'stepgen':5, 'analog':0, 'encoder':0, 'spinenc':1, 'spinana':1},
-			'7i77':{'axis':6, 'stepgen':0, 'analog':6, 'encoder':6, 'spinenc':1, 'spinana':1},
-			'7i78':{'axis':4, 'stepgen':4, 'analog':0, 'encoder':0, 'spinenc':1, 'spinana':1},
-			'7i85':{'axis':4, 'stepgen':4, 'analog':0, 'encoder':0, 'spinenc':1, 'spinana':1},
-			'7i85s':{'axis':4, 'stepgen':4, 'analog':0, 'encoder':4, 'spinenc':1, 'spinana':1}
+		cards = {
+			'7i76':{'axis':5, 'stepgen':5, 'analog':0, 'encoder':0, 'spinenc':1, 'spinana':1, 'inputs':32, 'outputs':16},
+			'7i77':{'axis':6, 'stepgen':0, 'analog':6, 'encoder':6, 'spinenc':1, 'spinana':1, 'inputs':32, 'outputs':16},
+			'7i78':{'axis':4, 'stepgen':4, 'analog':0, 'encoder':0, 'spinenc':1, 'spinana':1, 'inputs':0, 'outputs':0},
+			'7i85':{'axis':4, 'stepgen':4, 'analog':0, 'encoder':0, 'spinenc':1, 'spinana':1, 'inputs':0, 'outputs':0},
+			'7i85s':{'axis':4, 'stepgen':4, 'analog':0, 'encoder':4, 'spinenc':1, 'spinana':1, 'inputs':0, 'outputs':0}
 			}
 		parent.mainTW.setTabVisible(tab, True)
 		parent.mainTW.setTabText(tab, f'P{connector} {parent.sender().currentText()}')
@@ -22,6 +23,8 @@ def changed(parent):
 		encoder = cards[board]['encoder']
 		spinenc = cards[board]['spinenc']
 		spinana = cards[board]['spinana']
+		inputs = cards[board]['inputs']
+		outputs = cards[board]['outputs']
 
 		for i in range(1,7): # show/hide axis tabs
 			if i <= axis:
@@ -49,6 +52,22 @@ def changed(parent):
 
 		for i in range(32): # hide debounce check boxes
 			getattr(parent, f'c{daughter}_input_debounce_{i}').setEnabled(False)
+
+		for i in range(32): # enable/disable inputs
+			if inputs > 0 and i <= inputs:
+				getattr(parent, f'c{daughter}_input_{i}').setEnabled(True)
+				getattr(parent, f'c{daughter}_input_invert_{i}').setEnabled(True)
+			else:
+				getattr(parent, f'c{daughter}_input_{i}').setEnabled(False)
+				getattr(parent, f'c{daughter}_input_invert_{i}').setEnabled(False)
+
+		for i in range(16): # enable/disable outputs
+			if outputs > 0 and i <= outputs:
+				getattr(parent, f'c{daughter}_output_{i}').setEnabled(True)
+				getattr(parent, f'c{daughter}_output_invert_{i}').setEnabled(True)
+			else:
+				getattr(parent, f'c{daughter}_output_{i}').setEnabled(False)
+				getattr(parent, f'c{daughter}_output_invert_{i}').setEnabled(False)
 
 	else:
 		parent.mainTW.setTabVisible(4 + index, False)
