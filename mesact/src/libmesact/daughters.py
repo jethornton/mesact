d2b9f1d16985ce5d09939b6ffb2f8cfbf59cfb34
John Thornton <dev@gnipsel.com>
2023-10-01 10:55:14 -0500

    display correct tab name
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- mesact/src/libmesact/daughters.py
+++ mesact/src/libmesact/daughters.py
@@ -6,8 +6,11 @@ def changed(parent):
 		daughter = index + 1
 		board = parent.sender().currentData()
 		tab = int(parent.sender().objectName()[-1]) + 4
-		connector = int(parent.sender().objectName()[-1]) + 1
-		getattr(parent, f'c{connector}_JointTW').setTabText(0, parent.sender().currentText())
+		# daughterLB_0
+		card = parent.sender().objectName()[-1]
+		connector = getattr(parent, f'daughterLB_{card}').text()
+		# connector = int(parent.sender().objectName()[-1]) + 1
+		getattr(parent, f'c{card}_JointTW').setTabText(0, parent.sender().currentText())
 		cards = {
 			'7i76':{'axis':5, 'stepgen':5, 'analog':0, 'encoder':0, 'spinenc':1, 'spinana':1, 'inputs':32, 'outputs':16},
 			'7i77':{'axis':6, 'stepgen':0, 'analog':6, 'encoder':6, 'spinenc':1, 'spinana':1, 'inputs':32, 'outputs':16},
@@ -16,7 +19,7 @@ def changed(parent):
 			'7i85s':{'axis':4, 'stepgen':4, 'analog':0, 'encoder':4, 'spinenc':1, 'spinana':1, 'inputs':0, 'outputs':0}
 			}
 		parent.mainTW.setTabVisible(tab, True)
-		parent.mainTW.setTabText(tab, f'P{connector} {parent.sender().currentText()}')
+		parent.mainTW.setTabText(tab, f'{connector} {parent.sender().currentText()}')
 		axis = cards[board]['axis']
 		stepgen = cards[board]['stepgen']
 		analog = cards[board]['analog']
