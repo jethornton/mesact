b700c8b572e2020c474d5e5eb5603f4d1b5f1813
John Thornton <dev@gnipsel.com>
2025-08-11 10:55:20 -0500

    add spi reload
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- mesact/src/libmesact/flash.py
+++ mesact/src/libmesact/flash.py
@@ -317,11 +317,14 @@ def reload_board(parent):
 			return
 
 	elif parent.boardType == 'spi':
-		msg = ('The Reload Board Function\n'
-		'has not been programed yet for SPI\n'
-		'JT might need your help\n'
-		'getting this done')
-		dialogs.msg_ok(msg, 'title')
+		if not parent.password:
+			password = getPassword(parent)
+			parent.password = password
+		if parent.password != None:
+			spi_address = parent.address_cb.currentText()
+			cmd = ['sudo', '-S', 'mesaflash', '--device', parent.mesaflash_name, '--spi', '--addr', spi_address, '--reload']
+			p = Popen(cmd, stdin=PIPE, stderr=PIPE, stdout=PIPE, text=True)
+			prompt = p.communicate(parent.password + '\n')
 
 	elif parent.boardType == 'pci':
 		if not parent.password:
